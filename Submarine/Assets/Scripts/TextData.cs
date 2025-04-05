138a98d3c9cfb923516ac3d4d51b9844ac698296
Jamm <j@j,jj>
2025-04-05 16:11:40 +0300

    text bubble and text manager

---
+++ Submarine/Assets/Scripts/TextData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTextData", menuName = "Text System/TextData")]
+public class TextData : ScriptableObject
+{
+    public string character;
+    public string text;
+}
